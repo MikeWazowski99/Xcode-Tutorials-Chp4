8123c024f24f6833ef72492455ee0a8e7a3e8738
Michael Tapia <mtapia19@students.mchenry.edu>
2024-02-05 13:16:12 -0600

    Chp4 Xcode Tutorials

--- Xcode Tutorials Chp4/ViewController.swift
+++ Xcode Tutorials Chp4/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Properties
+//    @IBOutlet var labelResult: UILabel!
+    @IBOutlet var textDisplay: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    // MARK: Methods
+    //@IBAction func changeButton(_ sender: UIButton)
+//    { labelResult.text = textDisplay.text?.uppercased()
+//        print (labelResult.text!)
+    //}
 
 }
 
